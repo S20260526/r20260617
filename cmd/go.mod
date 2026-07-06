c2e0c0e8d2f59164482af2f1636a5f3349119c31
S20260526 <>
2026-07-06 19:05:09 +0300

    prometheus client added

--- cmd/go.mod
+++ cmd/go.mod
@@ -1,3 +1,5 @@
 module m20260618
 
 go 1.24
+
+require github.com/prometheus/client_golang v1.23.2 // indirect
--- cmd/main.go
+++ cmd/main.go
@@ -6,12 +6,41 @@ import (
 	"log/slog"
 	"net/http"
 	"os"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/collectors"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
+
+type Handler struct {
+	ih infra.Handler
+	ph http.Handler
+}
+
+func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.RequestURI == "/hcheck" {
+		return
+	} else if r.RequestURI == "/prometrics" {
+		h.ph.ServeHTTP(w, r)
+	} else {
+		h.ih.ServeHTTP(w, r)
+	}
+}
 
 func main() {
 	slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stderr, nil)))
 
-	h := infra.NewHandler(app.NewService())
+	pr := prometheus.NewRegistry()
+
+	pr.MustRegister(
+		collectors.NewGoCollector(),
+		collectors.NewProcessCollector(collectors.ProcessCollectorOpts{}),
+	)
+
+	h := Handler{
+		ih: infra.NewHandler(app.NewService()),
+		ph: promhttp.HandlerFor(pr, promhttp.HandlerOpts{Registry: pr}),
+	}
 
 	s := http.Server{
 		Addr:    ":8080",
