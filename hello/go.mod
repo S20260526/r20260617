ef8f07e271c451a2d8a5a1862f3be7cf3605bfcd
S20260526 <>
2026-07-14 14:37:40 +0300

    missing essential files

---
+++ hello/go.mod
@@ -0,0 +1,3 @@
+module m20260618-hello
+
+go 1.24.4
---
+++ hello/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log/slog"
+	"net/http"
+	"os"
+
+	"app"
+	"infra"
+)
+
+func main() {
+	slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stderr, nil)))
+
+	s := http.Server{
+		Addr:    ":8081",
+		Handler: infra.NewHandler(app.NewHelloService()),
+	}
+
+	slog.Info("server", "state", "started")
+
+	e := s.ListenAndServe()
+
+	if e != nil {
+		panic(e)
+	}
+}
